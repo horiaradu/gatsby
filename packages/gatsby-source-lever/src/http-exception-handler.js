ee9b375f4fbd5bc137e548ade4c1247c0eb7ff85
kale-io <cos+a@iuvo.ai>
2018-02-14 11:33:41 +0200

    No console.log Remove `console.log` statements.

--- packages/gatsby-source-lever/src/http-exception-handler.js
+++ packages/gatsby-source-lever/src/http-exception-handler.js
@@ -5,7 +5,6 @@
  */
 function httpExceptionHandler(e) {
   const { status, statusText, data: { message } } = e.response
-  console.log(`The server response was "${status} ${statusText}"`)
   if (message) {
     console.log(`Inner exception message : "${message}"`)
   }
